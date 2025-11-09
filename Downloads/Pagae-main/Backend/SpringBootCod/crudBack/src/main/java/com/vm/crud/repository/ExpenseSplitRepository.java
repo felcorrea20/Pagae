364be6a43636dfb0e0534e0075dc7a868131e0f1
Miguel Lima <144189866+MiguelLSJ@users.noreply.github.com>
2025-11-08 22:22:37 -0300

    Delete Downloads directory

--- Downloads/Pagae-main/Backend/SpringBootCod/crudBack/src/main/java/com/vm/crud/repository/ExpenseSplitRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vm.crud.repository;
-
-import com.vm.crud.models.ExpenseSplit;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExpenseSplitRepository extends JpaRepository<ExpenseSplit, Long> {
-     void deleteByExpenseId (Long expenseId);
-}
--- Downloads/Pagae/Backend/SpringBootCod/crudBack/src/main/java/com/vm/crud/repository/GroupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vm.crud.repository;
-
-import com.vm.crud.models.Group;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GroupRepository extends JpaRepository<Group, Long> {
-}
