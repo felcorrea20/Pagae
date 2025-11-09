6fd0fdae3005e89f362b0d2f24a16ae155509969
Miguel Lima <miguellimasantana2345@gmail.com>
2025-11-08 22:29:12 -0300

    Delete group + Remove member

---
+++ Downloads/Pagae/Backend/SpringBootCod/crudBack/src/main/java/com/vm/crud/models/Group.java
@@ -0,0 +1,59 @@
+package com.vm.crud.models;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "TB_GROUP")
+public class Group {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "group")
+    private List<GroupMember> members = new ArrayList<>();
+
+    @OneToMany(mappedBy = "group")
+    private List<Expense> expenses = new ArrayList<>();
+
+    // Construtor vazio (obrigatório no JPA)
+    public Group() {
+    }
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<GroupMember> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<GroupMember> members) {
+        this.members = members;
+    }
+
+    public List<Expense> getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(List<Expense> expenses) {
+        this.expenses = expenses;
+    }
+}
