c85841dcc6774fc1d0f3ffd5904b0d69403913d7
Miguel Lima <144189866+MiguelLSJ@users.noreply.github.com>
2025-11-08 22:32:13 -0300

    Delete Downloads directory

--- Downloads/Pagae/Backend/SpringBootCod/crudBack/src/main/java/com/vm/crud/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vm.crud.repository;
-
-import com.vm.crud.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
